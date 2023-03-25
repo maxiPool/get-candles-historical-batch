64cdc6c73c8208f427867ebf82ddd65dfca96029
maxiPool <maxime.berard@gmail.com>
2023-03-25 15:51:28 -0400

    feat: batch bat files

--- get-candles-batch-with-build.bat
+++ get-candles-batch-with-build.bat
@@ -1,3 +1,4 @@
 echo "Running get candles batch job" > batch-job-output.txt && ^
+.\mvnw package -DskipTests=true >> batch-job-output.txt && ^
 java -jar .\target\get-candles-historical-batch-0.0.1-SNAPSHOT.jar --spring.profiles.active=local >> batch-job-output.txt && ^
 echo "DONE!" >> batch-job-output.txt
