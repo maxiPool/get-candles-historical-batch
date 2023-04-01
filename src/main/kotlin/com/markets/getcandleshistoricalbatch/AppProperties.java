f1a48469e1fd9647948a2eab4e89a5e4fe00eec6
maxiPool <maxime.berard@gmail.com>
2023-04-01 09:50:47 -0400

    feat: add config: disable on days

---
+++ src/main/kotlin/com/markets/getcandleshistoricalbatch/AppProperties.java
@@ -0,0 +1,10 @@
+package com.markets.getcandleshistoricalbatch;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@ConfigurationProperties(prefix = "app")
+public record AppProperties(Set<DayOfWeek> disableOnDays) {
+}
--- src/main/kotlin/com/markets/getcandleshistoricalbatch/OnAppReadyManager.java
+++ src/main/kotlin/com/markets/getcandleshistoricalbatch/OnAppReadyManager.java
@@ -8,24 +8,48 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static java.util.stream.Collectors.joining;
+
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class OnAppReadyManager {
 
+  private static final ZoneId TORONTO_ZONE = ZoneId.of("America/Toronto");
+
+  private final AppProperties appProperties;
   private final V20Properties v20Properties;
   private final CandlestickService candlestickService;
 
   @EventListener
-  public void onAppReady(ApplicationReadyEvent ignored) {
-    var message = "OnAppReadyManager";
-    log.info("[LAUNCHING] {}", message);
+  public void onAppReady(ApplicationReadyEvent ignored) throws IOException {
+    log.info("[AppReady]");
+    exitIfDisabledDays();
 
     candles();
 
-    log.info("[DONE] {}", message);
+    log.info("[DONE]");
     System.exit(0);
+  }
+
+  private void exitIfDisabledDays() throws IOException {
+    var today = ZonedDateTime.now(TORONTO_ZONE).getDayOfWeek();
+    if (appProperties.disableOnDays().contains(today)) {
+      log.warn("App disabled by configuration on days: {}", appProperties.disableOnDays());
+
+      log.info("\n\n{}\n\n", Files.readAllLines(Paths.get("batch-job-output.txt")).stream().collect(joining("\n")));
+
+      log.warn("App disabled by configuration on days: {}", appProperties.disableOnDays());
+      log.info("[DONE]");
+      System.exit(0);
+    }
   }
 
   private void candles() {
