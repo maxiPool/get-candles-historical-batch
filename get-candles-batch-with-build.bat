a29cb838bae110c399ffa370447054aaab996e1a
maxiPool <maxime.berard@gmail.com>
2025-01-05 21:17:40 -0500

    deployment: new folder

--- get-candles-batch-with-build.bat
+++ get-candles-batch-with-build.bat
@@ -1,4 +1,5 @@
 echo "Running get candles batch job" && ^
 .\mvnw clean package -DskipTests=true && ^
+mv .\target\get-candles-historical-batch-0.0.1-SNAPSHOT.jar .\deployment\get-candles-historical-batch-0.0.1-SNAPSHOT.jar && ^
 java -jar .\target\get-candles-historical-batch-0.0.1-SNAPSHOT.jar --spring.profiles.active=local && ^
 echo "DONE!"
--- get-candles-batch.bat
+++ get-candles-batch.bat
@@ -1,3 +1,3 @@
 echo "Running get candles batch job" && ^
-java -jar .\target\get-candles-historical-batch-0.0.1-SNAPSHOT.jar --spring.profiles.active=local && ^
+java -jar .\deployment\get-candles-historical-batch-0.0.1-SNAPSHOT.jar --spring.profiles.active=local && ^
 echo "DONE!"
