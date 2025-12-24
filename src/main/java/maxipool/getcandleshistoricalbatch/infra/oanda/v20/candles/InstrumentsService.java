9af91d69a1cfffc203c8917065ea8247cbc60a06
maxiPool <84409780+maxiPool@users.noreply.github.com>
2025-12-23 19:22:17 -0500

    email when failed to get instruments

--- src/main/java/maxipool/getcandleshistoricalbatch/infra/oanda/v20/candles/InstrumentsService.java
+++ src/main/java/maxipool/getcandleshistoricalbatch/infra/oanda/v20/candles/InstrumentsService.java
@@ -3,6 +3,7 @@ package maxipool.getcandleshistoricalbatch.infra.oanda.v20.candles;
 import com.oanda.v20.primitives.Instrument;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maxipool.getcandleshistoricalbatch.email.EmailService;
 import maxipool.getcandleshistoricalbatch.infra.oanda.v20.candles.resource.OandaRestResource;
 import maxipool.getcandleshistoricalbatch.infra.oanda.v20.properties.V20Properties;
 import org.springframework.stereotype.Service;
@@ -16,12 +17,14 @@ public class InstrumentsService {
 
   private final OandaRestResource resource;
   private final V20Properties properties;
+  private final EmailService emailService;
 
   public List<Instrument> findAll() {
     try {
       return resource.getInstruments(properties.accountId().toString()).instruments();
     } catch (Exception e) {
       log.error("Error while trying to getInstruments", e);
+      emailService.sendEmail("Error while trying to getInstruments: %s".formatted(e.getMessage()));
       throw e;
     }
   }
